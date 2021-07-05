cf18bba99a23e8bbc7732cc33c26e4a554268c72
Damian Makki <damian@makki.pro>
2021-07-05 12:16:29 -0500

    format profile birthday

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,15 +3,17 @@ import ActionBar from '../action-bar/ActionBar.js';
 
 export default function Profile(props) {
 
+    let profileBirthday = new Date(props.data.created_at).toLocaleString('default', { month: 'long', day: 'numeric', year: 'numeric' });;
+
     return (
         <section className="loaded-profile">
             <aside className="meta">
-                <img src={props.data.avatar_url} alt="Profile." />
+                <img src={props.data.avatar_url} alt={`${props.data.name}'s profile.`} />
                 <h2>{props.data.name}</h2>
             </aside>
             <nav className="details">
                 <ul>
-                    <li>Profile Created <span className="count">{props.data.created_at}</span></li>
+                    <li>Profile Created <span className="count">{profileBirthday}</span></li>
                     <li>Total Shots <span className="count">{props.shotData.length}</span></li>
                 </ul>
                 <ActionBar />
